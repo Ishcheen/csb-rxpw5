a2eaf1203f9bd68357e4f8a48d77cef0dd20f539
Code Sandbox <hello@codesandbox.io>
2021-07-01 15:40:24 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4465120e91403faef756104c899cd567.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2f6d32127f52b721196b7dff9b26d8",
+    "url": "https://.github.io/csb-rxpw5/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "https://.github.io/csb-rxpw5/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "017c0747528d8500a59e",
+    "url": "https://.github.io/csb-rxpw5/static/js/main.1e73580c.chunk.js"
+  },
+  {
+    "revision": "8de3fbdb6bbd8ea2e756",
+    "url": "https://.github.io/csb-rxpw5/static/js/runtime-main.a22da032.js"
+  }
+]);
